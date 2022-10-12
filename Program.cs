68e6c4021a1d8ea2ed910e787ed71c8d453774ad
Vladimir <user@MacBook-Air-user.local>
2022-10-12 22:54:49 +0700

    виснет напрочь терминал

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@
 
 Console.WriteLine("Введите любое пятизначное число");
 int num = int.Parse(Console.ReadLine()!);
-if (num<10000 && num>100000) Console.WriteLine("Число не является пятизначным");
+if (num < 10000) ;
+else if (num > 100000) ;
+Console.WriteLine("Число не является пятизначным");
 string str = num.ToString();
-Console.WriteLine(str[2]);
+
+if(str[1] == str[5]) ;
+else if(str[2] == str[4]) ;
+Console.WriteLine("Число является ПАЛИНДРОМОМ"); 
+else
+    Console.WriteLine("Число не является палиндромом");
+
